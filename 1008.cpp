b10a16c347bb567b67535244b484f71afee361f7
Harshitsharmaa01 <143832289+Harshitsharmaa01@users.noreply.github.com>
2023-09-02 11:18:05 +0530

    Third Code

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int age;
+	float height;
+	age=21;
+	height=5.8;
+	printf("enter th age and height ");
+	
+	  scanf("%d%f",&age,&height);
+	    printf("age is %d and height is %f" ,age,height);
+}
